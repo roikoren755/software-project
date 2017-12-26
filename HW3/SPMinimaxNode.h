5c62d26adc0ce9d40d3ad2b613bab4e062d3e425
nadav352 <33449831+nadav352@users.noreply.github.com>
2017-12-26 15:07:39 +0200

    1

--- HW3/SPMinimaxNode.h
+++ HW3/SPMinimaxNode.h
@@ -1,6 +1,21 @@
 #ifndef SPMINIMAXNODE_H_
 #define SPMINIMAXNODE_H_
 
+typedef enum {
+	MM_SUCCESS,
+	MM_ERROR,
+} SP_MM_MASSAGE;
+
+typedef struct command_t {
+	SP_MM_MASSAGE mas;
+	int score;
+} MMreturn;
+
 //Put all decleartions and constants here
+int scoreBoard(SPFiarGame* src);
+MMreturn spEvalGame(SPFiarGame* game, unsigned int height, bool turn);
+MMreturn spMaxIndex(SPFiarGame* game);
+MMreturn spMaxScore(SPFiarGame* game, unsigned int height);
+MMreturn spMinScore(SPFiarGame* game, unsigned int height);
 
 #endif
