004d991ee35230cbc3d00bb8cf94ef64482ae97a
nadav352 <33449831+nadav352@users.noreply.github.com>
2017-12-27 12:18:31 +0200

    doc

--- HW3/SPMinimax.h
+++ HW3/SPMinimax.h
@@ -23,8 +23,43 @@
  */
 int spMinimaxSuggestMove(SPFiarGame* currentGame,
 		unsigned int maxDepth);
+/**
+ * Given a game state, this function creates the minimax tree up to
+ * a specified length given by maxDepth.
+ * specified length given by maxDepth. The current game state doesn't change
+ * by this function including the history of previous moves.
+ *
+ * @param currentGame - The current game state (the user has to check this isn't null)
+ * @param maxDepth - The maximum depth of the miniMax algorithm
+ * @return
+ * NULL if any allocation error occurred.
+ * On success the function returns a node representing the root of the tree.
+ *
+ */
 MM_Node* MMCreateTree(SPFiarGame* currentGame,unsigned int maxDepth);
+/**
+ * Given a node in the minimax tree, this function recursively creates childs for this node.
+ * the number of childs is according to the constant Node_Childs_Num defined in the minimaxnode header.
+ * for every possible move, the function creates a copy of the parent's game and attempts
+ * the move.
+ * if the move is invalid, the function creates an invalid child.
+ * if the move is a winning/losing/tie, the the function creates a child with height 0.
+ * otherwise, the function creates a child with height: (parent.height-1) and calls
+ * itself recursively on the new child.
+ * if the parent has height 0, the function does nothing.
+ *
+ * @param MM_Node* parent - The parent node (the user has to check this isn't null)
+ * @return
+ * 0 if any allocation error occurred.
+ * On success the function returns 1.
+ *
+ */
 int createNodeChilds(MM_Node* parent);
+/**
+ * Frees recursively all memory resources associated with the source tree. If the
+ * source root is NULL, then the function does nothing.
+ * @param src - the source root of the tree.
+ */
 void TreeDesrtoy(MM_Node* root);
 
 #endif
