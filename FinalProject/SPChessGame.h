56c8447a551efb0fec3bb9b2c9c47bfd0e502864
nadav352 <33449831+nadav352@users.noreply.github.com>
2018-03-14 14:11:48 +0200

    .

--- FinalProject/SPChessGame.h
+++ FinalProject/SPChessGame.h
@@ -120,12 +120,9 @@ int spChessGameGetColumnFromPosition(char position);
 
 int spChessGameGetRowFromPosition(char position);
 
-<<<<<<< HEAD
 int setMoveCoordinatesToInt(int curRow,int curCol, int destRow, int destCol);
 
 int setStepCoordinatesToInt(int destRow,int destCol, int capture, int threaten);
 
 
-=======
->>>>>>> 975194953dad0156def63248e1c17ca0e4307bc7
 #endif //SOFTWARE_PROJECT_SPCHESSGAME_H
